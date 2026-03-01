ba00f1ad800a79c1771fe8734fa188ff9c1fca42
akiega <akira.ega@gmail.com>
2026-03-01 18:58:30 +0900

    feat: remove unused SVG utility functions from diagram panel

--- src/diagramPanel.ts
+++ src/diagramPanel.ts
@@ -1,9 +1,6 @@
 import * as vscode from "vscode";
 import type { WebviewToExtMessage, ExtToWebviewMessage } from "./types";
 import {
-  resolveNewSvgPath,
-  saveSvgFile,
-  insertMarkdownLink,
   listTemplates,
   saveTemplate,
   loadTemplate,
